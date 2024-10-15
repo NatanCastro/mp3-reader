b7c28502ad29b455abada219f391147fc3fb3d6d
Natan Gabriel Almeida de Castro <castro.natan@outlook.com>
2024-10-15 19:22:07 -0300

    start of ID3v2 tag detector

--- src/main.c
+++ src/main.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define ID3V2_TAG_SIZE 10
+
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     printf("Please provide the path to the MP3 audio file\n");
@@ -20,14 +22,36 @@ int main(int argc, char *argv[]) {
     return EXIT_FAILURE;
   }
 
+  uint8_t id3v2_tag[ID3V2_TAG_SIZE] = {0};
+
+  if (fread(&id3v2_tag, 1, ID3V2_TAG_SIZE, file)) {
+    if (id3v2_tag[0] != 'I' || id3v2_tag[1] != 'D' || id3v2_tag[2] != '3') {
+      rewind(file);
+      printf("No id3v2 tag\n");
+      goto jump_ID3V2;
+    }
+
+    printf("version: %u.%u\n", id3v2_tag[3], id3v2_tag[4]);
+    printf("flags: %x\n", id3v2_tag[5]);
+    int length = (id3v2_tag[6] << 21) | (id3v2_tag[7] << 14) |
+                 (id3v2_tag[8] << 7) | (id3v2_tag[9]);
+    printf("tag length: %u\n", length);
+    printf("tag length: %x\n", length);
+    fseek(file, 10 + length, SEEK_SET);
+  }
+jump_ID3V2:
+  printf("\n");
+
   uint8_t header_buffer[4] = {0};
 
-  if (!fread(header_buffer, 1, 4, file)) {
+  if (fread(header_buffer, 1, 4, file) != 4) {
     printf("Could not read file: %s\nEmpty file provided\n", audio_file_path);
     return EXIT_FAILURE;
   }
 
-  MP3FrameHeader header = create_mp3_frame_header(header_buffer);
+  mp3_frame_header frame_header = create_mp3_frame_header(header_buffer);
+
+  print_header(&frame_header, NULL);
 
   fclose(file);
   return EXIT_SUCCESS;
